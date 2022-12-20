67828ccc41acddb030fdb95621933ef510729ec8
Yash D Rajput <yash.7232.rajput@gmail.com>
2022-12-20 20:11:33 +0530

    modified Reservations class

--- Home_app/models.py
+++ Home_app/models.py
@@ -7,11 +7,10 @@ class  Reservations(models.Model):
     phone=models.CharField(max_length=50)
     people=models.CharField(max_length=50)
     date=models.DateField()
-    # time=models.TextChoices('6:00 PM','7:00 PM','8:00 PM','9:00 PM','10:00 PM')
     time=models.CharField(default=6,max_length=5)
     message=models.TextField()
 
     def __str__(self) -> str:
-        return self.name+" "+self.people
+        return self.name+"  "+self.people
 
 
