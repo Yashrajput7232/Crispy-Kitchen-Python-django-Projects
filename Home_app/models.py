d98396b23643accf98ee620fcf37d5700aec39c9
Yash D Rajput <yash.7232.rajput@gmail.com>
2022-12-20 20:07:35 +0530

    displays name with number of person

--- Home_app/models.py
+++ Home_app/models.py
@@ -8,10 +8,10 @@ class  Reservations(models.Model):
     people=models.CharField(max_length=50)
     date=models.DateField()
     # time=models.TextChoices('6:00 PM','7:00 PM','8:00 PM','9:00 PM','10:00 PM')
-    # time=models.CharField(max_length=5)
+    time=models.CharField(default=6,max_length=5)
     message=models.TextField()
 
     def __str__(self) -> str:
-        return self.name
+        return self.name+" "+self.people
 
 
