988fd21d82ea4f362c7b29fbb0d0969aca77548c
Yash Dilip Rajput <95154909+Yashrajput7232@users.noreply.github.com>
2023-09-11 14:23:32 +0000

    added all configs

--- Hello/settings.py
+++ Hello/settings.py
@@ -130,7 +130,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 #added manualy
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
-STATICFILES_DIRS=[
-    os.path.join(BASE_DIR,"static")
-]
+# STATICFILES_DIRS=[
+#     os.path.join(BASE_DIR,"static"),
+    
+# ]
